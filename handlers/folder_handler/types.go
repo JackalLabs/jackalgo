163dd1869322f2be7ecf33ea681972cfd4494818
Marston Connell <34043723+TheMarstonConnell@users.noreply.github.com>
2023-07-10 20:20:13 -0400

    folder handler complete

--- handlers/folder_handler/types.go
+++ handlers/folder_handler/types.go
@@ -6,6 +6,7 @@ import (
 	"github.com/JackalLabs/jackalgo/handlers/wallet_handler"
 	"github.com/JackalLabs/jackalgo/types"
 	"github.com/JackalLabs/jackalgo/utils/compression"
+	"github.com/JackalLabs/jackalgo/utils/crypt"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
@@ -27,6 +28,18 @@ func NewFolderHandler(frame FolderFileFrame, wallet *wallet_handler.WalletHandle
 
 func TrackFolder(dirInfo FolderFileFrame, wallet *wallet_handler.WalletHandler) *FolderHandler {
 	return NewFolderHandler(dirInfo, wallet)
+}
+
+func TrackNewFolder(myName string, myParent string, myOwner string, wallet *wallet_handler.WalletHandler) *FolderHandler {
+	folderDetails := FolderFileFrame{
+		WhoAmI:       myName, // TODO : sanitize input
+		WhereAmI:     myParent,
+		WhoOwnsMe:    myOwner,
+		DirChildren:  make([]string, 0),
+		FileChildren: make(FolderChildFiles, 0),
+	}
+
+	return NewFolderHandler(folderDetails, wallet)
 }
 
 func (f *FolderHandler) GetWhoAmI() string {
@@ -63,6 +76,103 @@ func (f *FolderHandler) GetChildFiles() FolderChildFiles {
 
 func (f *FolderHandler) GetForFiletree() (sdk.Msg, error) {
 	return compression.SaveFiletreeEntry(f.walletHandler.GetAddress(), f.GetWhereAmI(), f.GetWhoAmI(), f.folderDetails, f.walletHandler)
+}
+
+func (f *FolderHandler) GetChildMerkle(child string) string {
+	return crypt.MerkleMeBro(fmt.Sprintf("%s/%s/%s", f.GetWhereAmI(), f.GetWhoAmI(), child))
+}
+
+func (f *FolderHandler) AddChildDirs(childNames []string) ([]sdk.Msg, []string, error) {
+	existing := make([]string, 0)
+	more := make([]string, 0)
+
+	for _, name := range childNames {
+		for _, childDir := range f.folderDetails.DirChildren {
+			if name == childDir {
+				existing = append(existing, name)
+				continue
+			}
+			more = append(more, name)
+		}
+	}
+
+	msgs := make([]sdk.Msg, len(more))
+	for i, moreName := range more {
+		myName, myPath, myOwner := f.MakeChildDirInfo(moreName)
+		tracker := TrackNewFolder(myName, myPath, myOwner, f.walletHandler)
+		ftree, err := tracker.GetForFiletree()
+		if err != nil {
+			return nil, nil, err
+		}
+		msgs[i] = ftree
+	}
+
+	if len(more) > 0 {
+		set := make(map[string]interface{})
+		for _, moreName := range more {
+			set[moreName] = nil
+		}
+		for _, dirChild := range f.folderDetails.DirChildren {
+			set[dirChild] = nil
+		}
+
+		children := make([]string, 0)
+		for key := range set {
+			children = append(children, key)
+		}
+
+		f.folderDetails.DirChildren = children
+		filetreeMsg, err := f.GetForFiletree()
+		if err != nil {
+			return nil, nil, err
+		}
+		msgs = append(msgs, filetreeMsg)
+	}
+
+	return msgs, existing, nil
+}
+
+func (f *FolderHandler) AddChildFileReferences(newFiles FolderChildFiles) (sdk.Msg, error) {
+	for key, value := range newFiles {
+		f.folderDetails.FileChildren[key] = value
+	}
+
+	return f.GetForFiletree()
+}
+
+func (f *FolderHandler) RemoveChildDirReferences(toRemove []string) (sdk.Msg, error) {
+	filtered := make([]string, 0)
+	for _, child := range f.folderDetails.DirChildren {
+		needsRemoved := false
+		for _, removing := range toRemove {
+			if child == removing {
+				needsRemoved = true
+				break
+			}
+		}
+		if !needsRemoved {
+			filtered = append(filtered, child)
+		}
+	}
+
+	f.folderDetails.DirChildren = filtered
+
+	return f.GetForFiletree()
+}
+
+func (f *FolderHandler) RemoveChildFileReferences(toRemove []string) (sdk.Msg, error) {
+	for _, removing := range toRemove {
+		delete(f.folderDetails.FileChildren, removing)
+	}
+
+	return f.GetForFiletree()
+}
+
+func (f *FolderHandler) MakeChildDirInfo(childName string) (myName string, myParent string, myOwner string) {
+	myName = childName
+	myParent = f.GetMyPath()
+	myOwner = f.folderDetails.WhoOwnsMe
+	return
 }
 
 type FolderFileFrame struct {
